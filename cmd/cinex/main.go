1512bc2d3fb1070fd8aa55bcf44cbf28befcddf6
user.shadow:"dishantyadav84gmail.com <dishantyadav84@gmail.com>
2026-03-03 17:26:22 +0530

    uwu

--- cmd/cinex/main.go
+++ cmd/cinex/main.go
@@ -4,15 +4,30 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/SssHhhAaaDddOoWww/cinex_/internal/tui"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	p := tea.NewProgram(InitialModel())
+	godotenv.Load()
+
+	p := tea.NewProgram(tui.InitialModel())
 	_, err := p.Run()
 	if err != nil {
 		fmt.Println("Error occured while starting !!!")
 		os.Exit(1)
 
 	}
+
+	// torrents, err := provider.GetTorrent("inception", "movie")
+	// if err != nil {
+	// 	fmt.Println("torrent error:", err)
+	// 	os.Exit(1)
+	// }
+
+	// for i, t := range torrents {
+	// 	fmt.Printf("[%d] %s | %s | seeds: %d\n", i+1, t.Title, t.Size, t.Seeders)
+	// }
+
 }
